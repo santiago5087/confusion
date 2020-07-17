89cdccb99e26434598ec9ab47b3f6ad488230371
Santiago Marín <esmarina@unal.edu.co>
2020-07-17 09:50:06 -0500

    snack bar added

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate } from '@angular/router';
+
 import { AuthService } from './auth.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +10,13 @@ import { AuthService } from './auth.service';
 export class AuthGuardService implements CanActivate{
 
   constructor(public auth: AuthService,
-    public router: Router) { }
+    public router: Router,
+    public snackBar: MatSnackBar) { }
 
     canActivate(): boolean {
       if (!this.auth.isLoggedIn()) {
         this.router.navigate(['home']);
+        this.snackBar.open("Only access to logged in users!", "Ok!", { duration: 6000 });
         return false;
       }
       return true;
